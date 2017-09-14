23c3d18335aba4a961a24df4277a0e3ea4647fac
quantumdot <thackray@rutgers.edu>
2017-09-14 09:42:33 -0400

    modifies .gitignore. made flaskCORS python 2/3 agnostic

--- server/flaskCORS.py
+++ server/flaskCORS.py
@@ -2,6 +2,11 @@ from datetime import timedelta
 from flask import Flask, make_response, request, current_app  
 from functools import update_wrapper
 
+try:
+  basestring
+except NameError:
+  basestring = str
+
 
 def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
     if methods is not None:
