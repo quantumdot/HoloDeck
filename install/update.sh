bac42d58d04857411346a745484728c53c2f77ac
quantumdot <thackray@rutgers.edu>
2017-09-28 17:25:58 -0400

    update update script

--- install/update.sh
+++ install/update.sh
@@ -8,5 +8,10 @@ git pull
 
 #cp install/holodeckd /etc/init.d/holodeckd
 #chmod 755 /etc/init.d/holodeckd
+sudo cp install/holodeck.service /lib/systemd/system/holodeck.service
+sudo chmod 644 /lib/systemd/system/holodeck.service
+sudo systemctl daemon-reload
+sudo systemctl enable holodeck.service
+sudo reboot
 
 cd "$prevdir"
